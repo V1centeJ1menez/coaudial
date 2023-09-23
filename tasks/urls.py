b1bf1be2cdb01692ce9acac4d7d3b5a233c3c8a5
V1centeJ1menez <vjimenez@usm.cl>
2023-09-23 20:08:40 -0300

    posible forma de implementacion de login, espero funcione

--- tasks/urls.py
+++ tasks/urls.py
@@ -4,7 +4,7 @@ from tasks import views
 urlpatterns = [
     path("", views.index, name='index'),
     path("signup/", views.signup, name="signup"),
-    path("login/", views.login, name="login"),
+    path("login/", views.login_view, name="login"),
     path("home/", views.home,name="home"),
     path("cursos/", views.cursos,name="cursos"),
     path('<str:username>/', views.profile, name='profile'),
